bddf0db5f2f25dc370f013fb0c7ce601005233c9
john33mcd <jmcd-34@hotmail.com>
2023-02-27 16:14:48 +0000

    test working with bare bones, first tutorial vid complete

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,17 @@
+const addition = require("../calc");
+describe("calculator", () => {
+    describe("Addition function", () => {
+        test("should return 42 for 20 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        })
+    });
+    describe("Subtraction function", () => {
+
+    });
+    describe("Multipication function", () => {
+
+    });
+    describe("Division function", () => {
+
+    });
+})
